af7b4502f46c03e165f5f26db3d2dea54c133063
IanB92 <reddox215@gmail.com>
2021-06-26 14:31:39 -0400

    second

---
+++ script.js
@@ -0,0 +1,44 @@
+const quizData = [
+   { question: 'Who was the first president of US?',
+    a: 'George Washington',
+    b: 'Anita Baker',
+    c: 'Georege Bush',
+    d: 'Booker T. Washington',
+    correct:'a',
+ },
+
+ {
+     question: 'Which wrestler was known for "the peoples eyebrow"?',
+     a: 'Stone Cold Steve Austin',
+     b: 'Triiple H',
+     c: 'The Undertaker',
+     d: 'The Rock',
+     correct: 'd'
+ },
+ {
+     question: 'Which subatomic particle contains a negative charge?',
+     a: 'protons',
+     b: 'electrons',
+     c: 'nuetrons',
+     d: 'subtrons',
+     correct: 'b'
+ },
+ {
+     question: 'Whats the capital of Missouri?',
+     a: 'Jefferson City',
+     b: 'Des Moines',
+     c: 'Juneau',
+     d: 'Annapolis',
+     correct: 'a'
+ },
+ {
+     question: 'What does DMX stand for?',
+     a: 'Dog Man X',
+     b: 'Dark Man X',
+     c: 'Dope Man X',
+     d: 'Dark Master X',
+     correct: 'b'
+ }
+
+
+]
